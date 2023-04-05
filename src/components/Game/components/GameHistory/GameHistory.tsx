39e57562c89b71176552989a49c4b9a69230bac5
Nikita1502 <58480269+Nikita1502@users.noreply.github.com>
2023-04-05 18:51:40 +0700

    added history of coeffiesients

--- src/components/Game/components/GameHistory/GameHistory.tsx
+++ src/components/Game/components/GameHistory/GameHistory.tsx
@@ -1,6 +1,59 @@
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 import styles from './GameHistory.module.scss'
+import classNames from 'classnames'
+import profilePicture from '../../../Players/img/dog.png'
 
-export const GameHistory: FC = () => {
-    return <div className={styles.wrapper}></div>
+interface IHistoryProps {
+    // coefficients: number[]
+    val: number
+    cfcolor: Tcolor
+    // cfHistory: object[]
+}
+
+export type Tcolor = 'red' | 'white' | 'orange' | 'green'
+
+const gradeCfMap: Record<Tcolor, string> = {
+    red: styles.zero_cf,
+    white: styles.sm_cf,
+    orange: styles.md_cf,
+    green: styles.lg_cf,
+}
+
+const coefficients = [12.12, 4.23, 1.22, 6.98, 0, 0, 4.01, 2.0, 0, 4.76]
+
+const coloredCfs: IHistoryProps[] = coefficients.map((element, index) => {
+    if (element == 0) {
+        return { val: element, cfcolor: 'red' }
+    } else if (element > 0 && element <= 2) {
+        return { val: element, cfcolor: 'white' }
+    } else if (element > 2 && element <= 5) {
+        return { val: element, cfcolor: 'orange' }
+    } else return { val: element, cfcolor: 'green' }
+})
+
+export const GameHistory = () => {
+    const cfHistory = useMemo(
+        () =>
+            coloredCfs.map((element, index) => {
+                const { val, cfcolor } = element
+                return (
+                    <span
+                        key={index}
+                        className={classNames(
+                            styles.coefficent,
+                            gradeCfMap[cfcolor]
+                        )}
+                    >
+                        {element.val} X
+                    </span>
+                )
+            }),
+        [coloredCfs]
+    )
+    return (
+        <div className={styles.wrapper}>
+            {/*<span className={styles.lg_cf}>17.28 X</span>*/}
+            {cfHistory}
+        </div>
+    )
 }
