e80f0623d9d3ddbe49e59ca5db75b3a30d2b49f8
idgrishin <kolyanchik2289@yandex.ru>
2023-04-19 15:42:14 +0300

    HUE-32/ ws test

--- src/components/ws/socket.ts
+++ src/components/ws/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const URL = 'http://localhost:6970'
+const URL = 'https://rocketbackend.up.railway.app:6970'
 
 export const socket = io(URL, {
     autoConnect: false,
